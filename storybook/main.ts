a876c97b3a15915e4bd772f95709717006d41d56
Will Klein <will@willklein.co>
2026-08-10 17:20:02 -0400

    Setup storybook-addon-playscape

--- storybook/main.ts
+++ storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['./stories/**/*.mdx', './stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs'],
+  addons: ['@storybook/addon-docs', 'storybook-addon-playscape'],
   framework: '@storybook/react-vite',
 }
 export default config
