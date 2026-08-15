afa9610058a8de9f62f2d20c902bfed3e70ac158
Will Klein <will@willklein.co>
2026-08-14 23:09:33 -0400

    Bump dependencies

--- storybook/main.ts
+++ storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['./stories/**/*.mdx', './stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs', 'storybook-addon-playscape'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-a11y', 'storybook-addon-playscape'],
   framework: '@storybook/react-vite',
 }
 export default config
